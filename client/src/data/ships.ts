69ea20d783e6d3e9777f7880e6ece88347aabd40
crypticmainioi <39269248-crypticmainioi@users.noreply.replit.com>
2025-02-01 19:00:42 +0000

    Agent query: Can you see the expanded ships list with all the new ships in the table? Is the filtering by ship class working correctly?
    Add new ship data to the ships array.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/d10c4d37-98c1-43f7-8ebb-5fce0aada533/9746b293-21de-4028-9e8f-7d040d45c8e1.jpg

--- client/src/data/ships.ts
+++ client/src/data/ships.ts
@@ -43,6 +43,115 @@ export const ships: Ship[] = [
     cargo: 25,
     speed: 265,
     manufacturer: "Paranid"
+  },
+  {
+    id: "nova",
+    name: "Nova Raider",
+    class: "Heavy Fighter",
+    size: "S",
+    price: 198_450,
+    crew: 2,
+    cargo: 30,
+    speed: 235,
+    manufacturer: "Split Dynasty"
+  },
+  {
+    id: "pegasus",
+    name: "Pegasus Sentinel",
+    class: "Corvette",
+    size: "M",
+    price: 892_750,
+    crew: 15,
+    cargo: 250,
+    speed: 175,
+    manufacturer: "Teladi Company"
+  },
+  {
+    id: "cerberus",
+    name: "Cerberus Sentinel",
+    class: "Frigate",
+    size: "M",
+    price: 1_456_800,
+    crew: 45,
+    cargo: 750,
+    speed: 145,
+    manufacturer: "Argon Federation"
+  },
+  {
+    id: "nemesis",
+    name: "Nemesis Vanguard",
+    class: "Destroyer",
+    size: "L",
+    price: 4_785_200,
+    crew: 85,
+    cargo: 1250,
+    speed: 95,
+    manufacturer: "Paranid"
+  },
+  {
+    id: "phoenix",
+    name: "Phoenix Carrier",
+    class: "Carrier",
+    size: "XL",
+    price: 12_450_000,
+    crew: 350,
+    cargo: 2500,
+    speed: 75,
+    manufacturer: "Split Dynasty"
+  },
+  {
+    id: "mercury",
+    name: "Mercury Hauler",
+    class: "Trader",
+    size: "M",
+    price: 425_750,
+    crew: 8,
+    cargo: 2750,
+    speed: 155,
+    manufacturer: "Teladi Company"
+  },
+  {
+    id: "magnetar",
+    name: "Magnetar Miner",
+    class: "Miner",
+    size: "M",
+    price: 385_900,
+    crew: 12,
+    cargo: 2250,
+    speed: 135,
+    manufacturer: "Argon Federation"
+  },
+  {
+    id: "raptor",
+    name: "Raptor Scout",
+    class: "Scout",
+    size: "S",
+    price: 82_450,
+    crew: 1,
+    cargo: 45,
+    speed: 255,
+    manufacturer: "Split Dynasty"
+  },
+  {
+    id: "hawk",
+    name: "Hawk Fighter",
+    class: "Fighter",
+    size: "S",
+    price: 156_800,
+    crew: 2,
+    cargo: 20,
+    speed: 275,
+    manufacturer: "Argon Federation"
+  },
+  {
+    id: "dragon",
+    name: "Dragon Raider",
+    class: "Heavy Fighter",
+    size: "S",
+    price: 212_750,
+    crew: 2,
+    cargo: 35,
+    speed: 225,
+    manufacturer: "Split Dynasty"
   }
-  // Add more ships as needed
 ];
